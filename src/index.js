61fe0d508a4c089cbfbb822a1d217a6cdcd9bdf1
Sam Markham <sam.e.markham@gmail.com>
2020-03-24 11:39:32 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const whenClicked = () => {
+  console.log("clicked!")
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={whenClicked} />
   </div>,
   document.getElementById('root')
 );
